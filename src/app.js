6611bd75d897d6d9507387001f429f6b309c6f8c
Alexandre Severo <alexandre.severo@me.com>
2020-04-06 17:47:12 -0300

    added middleware to validade repository id

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+function getRepoIndex(id) {
+  return repositories.findIndex(repo => repo.id === id);
+}
+
+function validateRepositoryId(request, response, next) {
+  const repoIndex = getRepoIndex(request.params.id);
+
+  if (repoIndex < 0)
+    return response.status(400).json({ error: "Invalid project Id"});
+
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
@@ -22,9 +37,6 @@ app.post("/repositories", (request, response) => {
 app.put("/repositories/:id", (request, response) => {
   const { title, url, techs } = request.body;
   const repoIndex = getRepoIndex(request.params.id);
-  
-  if (repoIndex < 0) 
-    return response.status(400).json({ error: "Repo not found"});
 
   const repo = repositories[repoIndex];
   repositories[repoIndex] = {
@@ -39,25 +51,14 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const repoIndex = getRepoIndex(request.params.id);
-  if (repoIndex < 0) 
-    return response.status(400).json({ error: "Repo not found"});
   repositories.splice(repoIndex, 1);
   return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const repoIndex = getRepoIndex(request.params.id);
-
-  if (repoIndex < 0) 
-    return response.status(400).json({ error: "Repo not found"});
-
   repositories[repoIndex].likes++;
-
   return response.json(repositories[repoIndex]);
 });
-
-function getRepoIndex(id) {
-  return repositories.findIndex(repo => repo.id === id);
-}
 
 module.exports = app;
